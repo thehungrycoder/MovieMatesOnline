0b55d946682fb77bb8a8576adc0eeb7f00e99c0f
HungryCoder <rajuru@gmail.com>
2012-05-01 13:29:20 +0600

    mongoid fixed

---
+++ app/models/movie.rb
@@ -0,0 +1,28 @@
+class Movie
+  include Mongoid::Document
+
+  field :name, :type => String
+  field :alternate_names, :type => String
+  field :imdbid, :type => String
+  field :released, :type => Integer
+  field :tagline, :type => String
+  field :overview, :type => String
+  field :imdb_rating, :type => String
+  field :certificate, :type => String
+  field :runtime, :type => String
+  field :cover_image, :type => String
+  field :display, :type => Boolean, :Default => 1
+  field :created_at, :type => Date, :Default => Time.now
+  field :updated_at, :type => Date
+
+  default_scope where(:display => 1)
+
+  #sunspot
+  include Sunspot::Mongo
+
+  searchable do
+    text :name
+    time :created_at
+  end
+
+end
